4bce7342fbfe0ccc299b6e4be23d17112e60be02
Emeka Mbazor <Emeka.Mbazor@wsj.com>
2021-01-30 13:49:52 -0500

    parse through item container to obtain specific details

--- scrape.py
+++ scrape.py
@@ -17,10 +17,37 @@ page_html = req.text
 page_htmls.append(page_html)
 
 # parse the html 
+# for page_html in page_htmls:
 page_soup = soup(page_html, 'html.parser')
 
 # find all item containers
 containers = page_soup.findAll('div', {'class': 's-item__info clearfix'})
 
-print(len(containers))
-print(containers[0])
+    #for container in containers:
+container = containers[0]
+
+# extract all info 
+name = container.find('h3', {'class': 's-item__title'}).string
+link = container.find('a')['href']
+price = container.find('span', {'class': 's-item__price'}).string
+
+rating_container = container.find('div', {'class': 'x-star-rating'})
+if rating_container is not None:
+    rating = rating_container.find('span', {'class':'clipped'}).string
+else: 
+    rating = None
+
+condition = container.find('span', {'class': 'SECONDARY_INFO'}).string
+
+sell_info = container.find('span', {'class': 'BOLD NEGATIVE'})
+if sell_info is not None:
+    sell_info = sell_info.string
+else:
+    sell_info = None 
+
+print(name)
+print(link)
+print(price)
+print(rating)
+print(condition)
+print(sell_info)
