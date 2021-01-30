e69ed7ccb9d8d39d711fee222c0d2092021acffb
Emeka Mbazor <Emeka.Mbazor@wsj.com>
2021-01-30 14:03:55 -0500

    obtain ten pages worth of item data with each scrape

--- scrape.py
+++ scrape.py
@@ -10,51 +10,51 @@ page_htmls = []
 names, links, prices, ratings, conditions, sell_infos = [], [], [], [], [], []
 
 # grab html 
-# for i in range(1, 11):
-i = 1
-print('page %d...' % i)
-url_base = 'https://www.ebay.com/sch/i.html?_dcat=139973&_sop=12&LH_TitleDesc=0&_fsrp=1&_sacat=139973&_nkw=switch&_from=R40&Region%2520Code=NTSC%252DU%252FC%2520%2528US%252FCanada%2529&Platform=Nintendo%2520Switch&LH_PrefLoc=1&rt=nc&_pgn='
-url = url_base + str(i)
-req = requests.get(url)
-page_html = req.text
-page_htmls.append(page_html)
+for i in range(1, 11):
+    print('page %d...' % i)
+    url_base = 'https://www.ebay.com/sch/i.html?_dcat=139973&_sop=12&LH_TitleDesc=0&_fsrp=1&_sacat=139973&_nkw=switch&_from=R40&Region%2520Code=NTSC%252DU%252FC%2520%2528US%252FCanada%2529&Platform=Nintendo%2520Switch&LH_PrefLoc=1&rt=nc&_pgn='
+    url = url_base + str(i)
+    req = requests.get(url)
+    page_html = req.text
+    page_htmls.append(page_html)
 
-# parse the html 
-# for page_html in page_htmls:
-page_soup = soup(page_html, 'html.parser')
+    # parse the html 
+    # for page_html in page_htmls:
+    page_soup = soup(page_html, 'html.parser')
 
-# find all item containers
-containers = page_soup.findAll('div', {'class': 's-item__info clearfix'})
+    # find all item containers
+    containers = page_soup.findAll('div', {'class': 's-item__info clearfix'})
 
-for container in containers:
+    for container in containers:
 
-    # extract all info 
-    name = container.find('h3', {'class': 's-item__title'}).string
-    link = container.find('a')['href']
-    price = container.find('span', {'class': 's-item__price'}).string
+        # extract all info 
+        name = container.find('h3', {'class': 's-item__title'}).string
+        link = container.find('a')['href']
+        price = container.find('span', {'class': 's-item__price'}).string
 
-    rating_container = container.find('div', {'class': 'x-star-rating'})
-    if rating_container is not None:
-        rating = rating_container.find('span', {'class':'clipped'}).string
-    else: 
-        rating = None
+        rating_container = container.find('div', {'class': 'x-star-rating'})
+        if rating_container is not None:
+            rating = rating_container.find('span', {'class':'clipped'}).string
+        else: 
+            rating = None
 
-    condition = container.find('span', {'class': 'SECONDARY_INFO'}).string
+        condition = container.find('span', {'class': 'SECONDARY_INFO'}).string
 
-    sell_info = container.find('span', {'class': 'BOLD NEGATIVE'})
-    if sell_info is not None:
-        sell_info = sell_info.string
-    else:
-        sell_info = None 
+        sell_info = container.find('span', {'class': 'BOLD NEGATIVE'})
+        if sell_info is not None:
+            sell_info = sell_info.string
+        else:
+            sell_info = None 
 
-    # put info into arrays
-    names.append(name)
-    links.append(link)
-    prices.append(price)
-    ratings.append(rating)
-    conditions.append(condition)
-    sell_infos.append(sell_info)
+        # put info into arrays
+        names.append(name)
+        links.append(link)
+        prices.append(price)
+        ratings.append(rating)
+        conditions.append(condition)
+        sell_infos.append(sell_info)
 
+# put lists into dictionary 
 df_dict = {'name': names,
            'link': links, 
            'price': prices,
@@ -62,6 +62,7 @@ df_dict = {'name': names,
            'condition': conditions,
            'sell_info': sell_infos}
 
+# convert dictionary to dataframe
 df = pd.DataFrame(df_dict)
 print(df.shape)
-print(df.head())
+print(df)
